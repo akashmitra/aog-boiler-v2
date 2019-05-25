c9502ff5457de7116e192692b56f18afb8cc51b7
Akash <akash.mitra@outlook.com>
2019-05-25 23:27:31 +0530

    Update and Clean Boilerplate for AOG, Dialogflow.
    Update and Clean Boilerplate for AOG, Dialogflow.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  log_filename:'dev.log',
-  searchResortPagination_count:3,
-  pagination_count_vacation:1,
-  api_endpoint:'https://aog-chatbot-server.glitch.me/'
+  log_filename: 'dev.log',
+  searchResortPagination_count: 3,
+  pagination_count_vacation: 1,
+  api_endpoint: 'https://aog-chatbot-server.glitch.me/'
 };
--- server.js
+++ server.js
@@ -1,193 +1,97 @@
-(function () {
-  'use strict';
+'use strict';
 
-  const express = require('express');
-  const bodyParser = require('body-parser');
-  const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
 
-  const { WebhookClient } = require('dialogflow-fulfillment');
-  const { Carousel, Image } = require('actions-on-google');
+const { WebhookClient } = require('dialogflow-fulfillment');
+const { Card, Suggestion } = require('dialogflow-fulfillment');
 
-  const logger = require('./log');
-  const UTIL = require('./util/util');
-  const ERROR_UTIL = require('./util/errorUtil');
-  const SESSION_UTIL = require('./util/sessionUtil');
-  const welcome_service = require('./services/welcome_service');
-  const test_service = require('./services/ssml.service');
-  const searchResort_service = require('./services/searchResort_service');
-  const deposit_service = require('./services/deposit_service');
-  const confirmedVacation_service = require('./services/confirmedVacation_service');
-  const goodbye_service = require('./services/goodbye_service');
-  const fallback_service = require('./services/fallback_service');
+/*---------------------- Utils ----------------------*/
+const logger = require('./log');
+const UTIL = require('./util/util');
+const ERROR_UTIL = require('./util/errorUtil');
+/*---------------------- Utils ----------------------*/
 
-  const port = process.env.PORT || 3000;
-  process.env.DEBUG = 'dialogflow:debug';
+/*---------------------- Intents ----------------------*/
+const welcome_intent = require('./intents/welcome.intent');
+const fallback_intent = require('./intents/fallback.intent');
+const exit_intent = require('./intents/exit.intent');
+/*---------------------- Intents ----------------------*/
 
-  const app = express();
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({
+/*---------------------- Services ----------------------*/
+const exit_service = require('./services/exit.service');
+const fallback_service = require('./services/fallback.service');
+/*---------------------- Services ----------------------*/
+
+
+const port = process.env.PORT || 3000;
+process.env.DEBUG = 'dialogflow:debug';
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
     extended: true
-  }));
+}));
 
 
-  app.post('/', (request, response) => {
-    const agent = new WebhookClient({ request: request, response: response });
-    logger.trace('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-    logger.trace('Dialogflow Request body: ' + JSON.stringify(request.body));
+app.post('/', (request, response) => {
+    const agent = new WebhookClient({ request, response });
+    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
-    var sessionID = UTIL.fetchSessionID(request);
-    var buildResponse = UTIL.buildResponse;
-
-    console.log(request.body);
-    console.log(sessionID);
-
-    function welcome() {
-      try {
-        return new Promise((resolve, reject) => {
-          welcome_service.welcome(agent, buildResponse, resolve);
-        });
-      }
-      catch (exception) {
-        ERROR_UTIL.serverError(exception, agent);
-      }
+    function welcome(agent) {
+        agent.add(`Welcome to my agent!`);
     }
 
-    function fallback() {
-      try {
-        let conv = agent.conv();
-        agent.add(fallback_service.fallback(conv));
-      }
-      catch (exception) {
-        ERROR_UTIL.serverError(exception, agent);
-      }
+    function fallback(agent) {
+        agent.add(`I didn't understand`);
+        agent.add(`I'm sorry, can you try again?`);
     }
 
-    function searchResort() {
-      try {
-        let location = request.body.queryResult.parameters.geostate;
-        let checkin_date = request.body.queryResult.parameters.checkin;
-        let checkout_date = request.body.queryResult.parameters.checkout;
-
-        let conv = agent.conv();
-        // conv.user.storage.startResortIndex=0;        //Reset to beginning
-        // conv.user.storage.resortLocation=location;    //Store location in session
-        // conv.user.storage.checkin_date=checkin_date;
-        // conv.user.storage.checkout_date=checkout_date;
-
-        SESSION_UTIL.setSession(sessionID, 'startResortIndex', 0);          //Reset to beginning
-        SESSION_UTIL.setSession(sessionID, 'resortLocation', location);     //Store location in session
-        SESSION_UTIL.setSession(sessionID, 'checkin_date', checkin_date);
-        SESSION_UTIL.setSession(sessionID, 'checkout_date', checkout_date);
-
-        return new Promise((resolve, reject) => {
-          searchResort_service.searchResort({ checkin_date, checkout_date, location },
-            sessionID, agent, buildResponse, resolve);
-        });
-      }
-      catch (exception) {
-        ERROR_UTIL.serverError(exception, agent);
-      }
+    function yourFunctionHandler(agent) {
+        agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
+        agent.add(new Card({
+            title: `Title: this is a card title`,
+            imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
+            text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+            buttonText: 'This is a button',
+            buttonUrl: 'https://assistant.google.com/'
+        })
+        );
+        agent.add(new Suggestion(`Quick Reply`));
+        agent.add(new Suggestion(`Suggestion`));
+        agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' } });
     }
 
-    function searchResortNext() {
-      try {
-        let conv = agent.conv();
-        searchResort_service.searchResort(conv.user.storage.checkin_date,
-          conv.user.storage.checkout_date,
-          conv.user.storage.resortLocation,
-          conv);
-        agent.add(conv);
-      }
-      catch (exception) {
-        ERROR_UTIL.serverError(exception, agent);
-      }
+    function googleAssistantHandler(agent) {
+        let conv = agent.conv(); // Get Actions on Google library conv instance
+        conv.ask('Hello from the Actions on Google client library!'); // Use Actions on Google library
+        agent.add(conv); // Add Actions on Google library responses to your agent's response
     }
 
-    function depositTradingPower() {
-      try {
-        let conv = agent.conv();
-        agent.add(deposit_service.depositTradingPower(conv));
-      }
-      catch (exception) {
-        ERROR_UTIL.serverError(exception, agent);
-      }
-    }
+    // See https://github.com/dialogflow/dialogflow-fulfillment-nodejs/tree/master/samples/actions-on-google
+    // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
 
-    function depositDetails() {
-      try {
-        let conv = agent.conv();
-        deposit_service.depositDetails(conv);
-        agent.add(conv);
-      }
-      catch (exception) {
-        ERROR_UTIL.serverError(exception, agent);
-      }
-    }
-
-    function upcomingVacation() {
-      try {
-        let conv = agent.conv();
-        conv.user.storage.startResortIndex = 0;
-        confirmedVacation_service.showUpcomingVacation(conv);
-        agent.add(conv);
-      }
-      catch (exception) {
-        ERROR_UTIL.serverError(exception, agent);
-      }
-    }
-
-    function upcomingVacationNext() {
-      try {
-        let conv = agent.conv();
-        confirmedVacation_service.showUpcomingVacation(conv);
-        agent.add(conv);
-      }
-      catch (exception) {
-        ERROR_UTIL.serverError(exception, agent);
-      }
-    }
-
-    function exit(agent) {
-      agent.add(goodbye_service.exit());
-    }
-
-    function testWebhook(agent) {
-      try {
-        return new Promise((resolve, reject) => {
-          test_service.test(agent, buildResponse, resolve);
-        });
-      }
-      catch (exception) {
-        ERROR_UTIL.serverError(exception, agent);
-      }
-    }
-
-
-    // Dialogflow intent Function Mapping
     let intentMap = new Map();
     intentMap.set('Default Welcome Intent', welcome);
     intentMap.set('Default Fallback Intent', fallback);
-    intentMap.set('Search Resort', searchResort);
-    intentMap.set('Search Resort Next', searchResortNext);
-    intentMap.set('Deposit Trading Power', depositTradingPower);
-    intentMap.set('Deposit Details', depositDetails);
-    intentMap.set('Vacation Details', upcomingVacation);
-    intentMap.set('Vacation Details Next', upcomingVacationNext);
-    intentMap.set('Exit', exit);
-    intentMap.set('TestWebhook', testWebhook);
-
+    intentMap.set('your intent name here', yourFunctionHandler);
+    intentMap.set('your intent name here', googleAssistantHandler);
     agent.handleRequest(intentMap);
-  });
 
 
-  app.get('/', (request, response) => {
+});
+
+
+app.get('/', (request, response) => {
     response.send('Say hello to AOG Chatbot! Running on port: ' + process.env.PORT);
     logger.trace(`Get hit!`);
-  });
+});
 
 
-  // Creating a server at port 
-  http.createServer(app).listen(port, () => {
+// Creating a server at port 
+http.createServer(app).listen(port, () => {
     logger.trace(`Express server listening on port ${port}`);
-  });
-}());
+});
+
